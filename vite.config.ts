ff49035ac2f878b33889c08c6286dc436844299c
bridgertandy <bridgertandy@gmail.com>
2026-07-29 15:55:07 -0600

    feat: add gh-pages for deployment and update dependencies
    - Added "gh-pages" dependency to facilitate deployment.
    - Updated package.json with a new "deploy" script to build and deploy the app.
    - Modified App.css to change the layout of the contact section to a grid and added hover effects.
    - Updated index.css to comment out the border-inline property.
    - Cleaned up formatting in vite.config.ts for consistency.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    babel({ presets: [reactCompilerPreset()] })
-  ],
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
   server: {
     open: true,
-  }
-})
+  },
+  base: "https://github.com/bridgertandy/GoldenPaws.git",
+});
